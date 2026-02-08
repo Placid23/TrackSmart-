dab135bd38ba916eb62d7e547a3d743f51880fc2
Kingsley Enyinnaya <enyikings3778@gmail.com>
2026-02-08 22:41:23 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSMWDl82-Meu7KjvuWQo2Xd5fyPUxy4Rg",
+  authDomain: "studio-1265903108-da197.firebaseapp.com",
+  projectId: "studio-1265903108-da197",
+  storageBucket: "studio-1265903108-da197.appspot.com",
+  messagingSenderId: "945116550304",
+  appId: "1:945116550304:web:d1503a83d0b83dbdabe5c5"
+};
